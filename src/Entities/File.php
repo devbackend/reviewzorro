7de7be58dcdc84ac686bc192abb895d1072d3348
Krivonos Ivan <devbackend@yandex.ru>
2019-05-11 13:22:43 +1000

    Merge branch 'master' of https://github.com/devbackend/reviewzorro
    # Conflicts:
    #	tests/Unit/Components/CollectionTest.php

---
+++ src/Entities/File.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ReviewZorro\Entities;
+
+use InvalidArgumentException;
+
+/**
+ * Entity for files.
+ *
+ * @author Ivan Krivonos <devbackend@yandex.ru>
+ */
+class File
+{
+	/** @var string */
+	private $path;
+
+	/**
+	 * @param string $path
+	 *
+	 * @author Ivan Krivonos <devbackend@yandex.ru>
+	 */
+	public function __construct(string $path)
+	{
+		if ('' === $path) {
+			throw new InvalidArgumentException('File path can\'t be empty');
+		}
+
+		$this->path = $path;
+	}
+
+	/**
+	 * @return string|null
+	 *
+	 * @author Ivan Krivonos <devbackend@yandex.ru>
+	 */
+	public function getExtension(): ?string
+	{
+		$parts = explode('.', $this->path);
+
+		if (1 === count($parts)) {
+			return null;
+		}
+
+		return end($parts);
+	}
+
+	/**
+	 * @param bool $withExtension
+	 *
+	 * @return string
+	 *
+	 * @author Ivan Krivonos <devbackend@yandex.ru>
+	 */
+	public function getName(bool $withExtension = true): string
+	{
+		$parts = explode(DIRECTORY_SEPARATOR, $this->path);
+
+		$result = end($parts);
+		if (false === $withExtension) {
+			$result = str_replace('.'.$this->getExtension(), '', $result);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @return string
+	 *
+	 * @author Ivan Krivonos <devbackend@yandex.ru>
+	 */
+	public function getPath(): string
+	{
+		return $this->path;
+	}
+}
