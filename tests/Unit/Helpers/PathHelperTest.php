cbe459341b2c2d359011c271fcff8f7e59c255fd
Krivonos Ivan <devbackend@yandex.ru>
2019-05-11 11:39:34 +1000

    Add PathHelper

---
+++ tests/Unit/Helpers/PathHelperTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ReviewZorro\Unit\Helpers;
+
+use PHPUnit\Framework\TestCase;
+use ReviewZorro\Helpers\PathHelper;
+
+/**
+ * Tests for PathHelper.
+ *
+ * @author Ivan Krivonos <devbackend@yandex.ru>
+ */
+class PathHelperTest extends TestCase
+{
+	/** @var string */
+	private $root;
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @author Ivan Krivonos <devbackend@yandex.ru>
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->root = dirname(dirname(dirname(__DIR__)));
+	}
+
+	public function testRootShouldReturnRootPathOfApp()
+	{
+		static::assertEquals($this->root, PathHelper::root());
+	}
+
+	public function testPathShouldReturnAbsolutePathForRelativeParam()
+	{
+		static::assertEquals($this->root . '/tests', PathHelper::path('tests'));
+	}
+
+	public function testPathShouldReturnRootPathForEmptyRelative()
+	{
+		static::assertEquals($this->root, PathHelper::path(''));
+	}
+}
